e936f06c8e3537db4591543e9d70d19ed6fbd4e2
JustinEvansUNCG <jtevans5@uncg.edu>
2025-02-21 17:02:49 -0500

    Package format set up

--- env/eduplan/forms.py
+++ env/eduplan/forms.py
@@ -1,1 +1,10 @@
-from __main__ import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+
+todos = ["Learn Flask", "Setup venv", "Build a cool app"]
+
+#basic flask form
+class TodoForm(FlaskForm):
+    todo = StringField("Todo")
+    submit = SubmitField("Add Todo")
--- env/eduplan/routes.py
+++ env/eduplan/routes.py
@@ -1,15 +1,10 @@
 from flask import render_template, request
 from eduplan import app
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from eduplan.forms import TodoForm, todos
 
 
-todos = ["Learn Flask", "Setup venv", "Build a cool app"]
 
-#basic flask form
-class TodoForm(FlaskForm):
-    todo = StringField("Todo")
-    submit = SubmitField("Add Todo")
+
 
 
 
