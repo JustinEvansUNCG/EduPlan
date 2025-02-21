279d79d336dd523feb08b3551685b6634b4f5a6a
JustinEvansUNCG <jtevans5@uncg.edu>
2025-02-21 16:51:17 -0500

    Package format created

--- env/eduplan/routes.py
+++ env/eduplan/routes.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request
+from flask import render_template, request
+from eduplan import app
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-
-app = Flask(__name__)
-
-# This is needed to use forms, but will be kept in a more secret place at a later date
-app.secret_key="anystringhere"
 
 todos = ["Learn Flask", "Setup venv", "Build a cool app"]
 
@@ -15,6 +11,9 @@ class TodoForm(FlaskForm):
     todo = StringField("Todo")
     submit = SubmitField("Add Todo")
 
+
+
+
 #basic index page that allows a user to add an item to a list
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -22,5 +21,3 @@ def index():
         todos.append(request.form['todo'])
     #render_template allows loading of an html file with other data such as lists, and is how we can add its to the todos variable defined above
     return render_template('index.html', todos = todos, template_form = TodoForm())
-
-
