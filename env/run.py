31d5834f08ea182a5240fa704d69f0f0f6a984cb
PradhyKothapalli <pkkothapall@uncg.edu>
2025-02-25 11:44:48 -0500

    DB setup

--- env/run.py
+++ env/run.py
@@ -1,4 +1,28 @@
 from eduplan import app
+import psycopg2
+
+conn = psycopg2.connect(
+    host = "localhost",
+    database = "CSC_490_Capstone",
+    user = "postgres",
+    password = "definitelynotthepassword"
+)
+
+cur = conn.cursor()
+insert_query = """
+INSERT INTO users (email, password_hash, role) 
+VALUES (%s, %s, %s)
+RETURNING user_id;
+"""
+user_data = ("43@example.com", "testpassword123", "student")
+cur.execute(insert_query,user_data)
+new_user_id = cur.fetchone()[0]
+print(f"Inserted user with ID: {new_user_id}")
+conn.commit()
+cur.close()
+conn.close()
+
+
 
 
 if __name__ == '__main__':
