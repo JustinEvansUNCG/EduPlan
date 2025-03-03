75ddd3692f53ec13b8a691eb756ab37630ff9412
PradhyKothapalli <pkkothapall@uncg.edu>
2025-03-02 19:51:13 -0500

    Works for now but need to fix more stuff(probably)

--- env/eduplan/models.py
+++ env/eduplan/models.py
@@ -1,13 +1,9 @@
-from eduplan import app
-import psycopg2
-import json
-import os
+from eduplan import db  
 
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_login import UserMixin
 from datetime import datetime
 
-db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
